686915a2ef0c78dc43066c1aeefd85f4109d34f2
Sachi Wagaarachchi <xcorat@gmail.com>
2026-02-21 05:20:26 -0800

    fix(gpui): embed toolbar icon assets

--- src/bin/araliya-gpui/main.rs
+++ src/bin/araliya-gpui/main.rs
@@ -4,14 +4,48 @@ mod components;
 
 use gpui::*;
 use gpui_component::Root;
-use std::sync::Arc;
+use std::{borrow::Cow, sync::Arc};
 
 use crate::api::ApiClient;
 use crate::state::{AppState, load_layout_prefs};
 use crate::components::AppView;
 
+#[derive(Clone, Copy, Debug, Default)]
+struct GpuiAssets;
+
+impl AssetSource for GpuiAssets {
+    fn load(&self, path: &str) -> Result<Option<Cow<'static, [u8]>>> {
+        let bytes: Option<&'static [u8]> = match path {
+            "icons/bot.svg" => Some(include_bytes!("icons/bot.svg")),
+            "icons/inbox.svg" => Some(include_bytes!("icons/inbox.svg")),
+            "icons/square-terminal.svg" => Some(include_bytes!("icons/square-terminal.svg")),
+            "icons/chart-pie.svg" => Some(include_bytes!("icons/chart-pie.svg")),
+            "icons/settings-2.svg" => Some(include_bytes!("icons/settings-2.svg")),
+            "icons/book-open.svg" => Some(include_bytes!("icons/book-open.svg")),
+            _ => None,
+        };
+
+        Ok(bytes.map(Cow::Borrowed))
+    }
+
+    fn list(&self, path: &str) -> Result<Vec<SharedString>> {
+        if path != "icons" {
+            return Ok(vec![]);
+        }
+
+        Ok(vec![
+            "bot.svg".into(),
+            "inbox.svg".into(),
+            "square-terminal.svg".into(),
+            "chart-pie.svg".into(),
+            "settings-2.svg".into(),
+            "book-open.svg".into(),
+        ])
+    }
+}
+
 fn main() {
-    let app = Application::new();
+    let app = Application::new().with_assets(GpuiAssets);
 
     app.run(move |cx| {
         gpui_component::init(cx);
